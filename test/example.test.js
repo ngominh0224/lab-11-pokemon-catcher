02dd589c2b7fb4ff9d5045e3b3ccb3ec4673ce6d
Minh Ngo <ngo.minh0224@gmail.com>
2021-01-27 00:42:50 -0800

    added results page and charts

--- test/example.test.js
+++ test/example.test.js
@@ -7,7 +7,7 @@ test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
+
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = false;
