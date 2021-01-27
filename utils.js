28b27adb116516e422f62960c0892036f7bc5e67
Minh Ngo <ngo.minh0224@gmail.com>
2021-01-26 23:34:37 -0800

    changes ti locale storage

--- utils.js
+++ utils.js
@@ -1,7 +1,13 @@
 import pokemon from './pokemon.js';
 
-export function getRandomPokemon(array) {
+export function getRandomPokemon() {
     const randomIndex = Math.floor(Math.random() * pokemon.length);
 
     return pokemon[randomIndex];
+}
+
+export function findByUnderscoreId(array, id) {
+    for (item of array) {
+        if (item._id === id) return item;
+    }
 }
